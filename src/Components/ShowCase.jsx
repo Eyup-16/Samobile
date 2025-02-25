96f748852bf12e8a060fb0fa3f895bab88e77da4
Eyup <171042578+Eyup-16@users.noreply.github.com>
2025-02-25 10:54:36 +0100

    Update ShowCase.jsx

--- src/Components/ShowCase.jsx
+++ src/Components/ShowCase.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 function ShowCase() {
   return (
-    <section className='relative h-screen flex items-center justify-center text-white shadow-[inset_0px_2px_4px_0px_rgba(0,_0,_0,_0.3)]'>
+    <section className='relative min-h-screen flex items-center justify-center text-white shadow-[inset_0px_2px_4px_0px_rgba(0,_0,_0,_0.3)]'>
   <div className="absolute inset-0 bg-[url(/repearing.jpg)] bg-cover bg-center before:absolute before:inset-0 before:backdrop-blur-md before:bg-black/50"></div>
   
-  <div className="absolute w-full max-w-screen-xl px-4 flex justify-center"> {/* Container for alignment */}
+  <div className="relative w-full max-w-screen-xl px-4 flex justify-center"> {/* Container for alignment */}
     <div className="flex gap-4 flex-wrap flex-col max-w-3xl text-center items-center "> {/* Flex parent for h1/p */}
       <span></span>
-      <span className="text-lg font-semibold text-[#CACBCD] "> {/* Small span above h1 */}
+      <span className="text-lg font-semibold text-[#CACBCD]  span"> {/* Small span above h1 */}
         Your Trusted Phone Experts
       </span>
       <h1 className='relative text-[40px] font-bold z-10  leading-tight'> {/* Larger h1 */}
