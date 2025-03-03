24a518b38e70242b3d2704d746841feec364b1a8
Eyup <171042578+Eyup-16@users.noreply.github.com>
2025-03-03 18:06:16 +0100

    Create Contact.tsx

---
+++ src/Components/Contact.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <section className=' bg-[#212121] p-8'>
+        {/* Header + description */}
+        <div className="text-center">
+
+    <h1 className='text-4xl font-semibold text-[#CACBCD] text-center mb-8 py-2 border-b-2 inline-block'>Contact Us</h1>
+    <span className="text-[18px] font-semibold text-[#CACBCD]  cursor-pointer block">
+        Let us know how we can help.</span>
+        </div>
+    </section>
+  )
+}
+
+export default Contact
