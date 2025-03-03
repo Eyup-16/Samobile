c31b9503fade2b206c2b4e6fdfec55f72c13cd89
Eyup <171042578+Eyup-16@users.noreply.github.com>
2025-03-03 18:06:40 +0100

    Create About.tsx

---
+++ src/Components/About.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function About() {
+  return (
+    <section className=' bg-[#212118] p-8'>
+    {/* Header + description */}
+    <div className="text-center">
+
+<h1 className='text-4xl font-semibold text-[#CACBCD] text-center mb-8 py-2 border-b-2 inline-block'>About Our Store</h1>
+<span className="text-[18px] font-semibold text-[#CACBCD]  cursor-pointer block">
+    See how we began & Who our team is.</span>
+    </div>
+</section>
+  )
+}
+
+export default About
