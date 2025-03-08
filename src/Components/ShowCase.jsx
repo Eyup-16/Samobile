6a4b346be69440b7753ed0905767d8b3b7086011
Eyup <171042578+Eyup-16@users.noreply.github.com>
2025-03-08 12:46:55 +0100

    Update ShowCase.jsx
    Need small changes

--- src/Components/ShowCase.jsx
+++ src/Components/ShowCase.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-
+import { Element } from 'react-scroll';
 function ShowCase() {
   return (
+    <Element name='Home'>
     <section className='relative min-h-screen flex items-center justify-center text-white shadow-[inset_0px_2px_4px_0px_rgba(0,_0,_0,_0.3)]'>
   <div className="absolute inset-0 bg-[url(/repearing.jpg)] bg-cover bg-center before:absolute before:inset-0 before:backdrop-blur-md before:bg-black/50"></div>
   
   <div className="relative w-full max-w-screen-xl px-4 flex justify-center"> {/* Container for alignment */}
     <div className="flex gap-4 flex-wrap flex-col max-w-3xl text-center items-center "> {/* Flex parent for h1/p */}
       <span></span>
-      <span className="text-[14px] font-semibold text-[#CACBCD]  span"> {/* Small span above h1 */}
+      <span className="text-[14px] font-semibold text-[#CACBCD] span cursor-pointer"> {/* Small span above h1 */}
         YOUR TRUSTED PHONE EXPERTS
       </span>
       <h1 className='relative text-[38px] font-bold z-10  leading-tight max-w-[500px] mb-3 font-["Roboto",_serif]'> {/* Larger h1 */}
@@ -23,6 +24,7 @@ function ShowCase() {
     </div>
   </div>
 </section>
+</Element>
   )
 }
 
