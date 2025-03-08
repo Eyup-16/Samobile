752df89809a3443b2c5ec03728010d3fa86f49be
Eyup <171042578+Eyup-16@users.noreply.github.com>
2025-03-08 12:44:07 +0100

    Update Contact.tsx
    one before latest construction

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -1,28 +1,67 @@
-import React, { HtmlHTMLAttributes, useRef } from 'react'
+import React, { useRef ,useState} from 'react'
 import { PhoneCall,MapPin ,MessagesSquare,Mail,Instagram} from 'lucide-react'
+import { Element } from 'react-scroll';
 function Contact() {
+  
+  //  State Variables
+  const [services, setServices] =useState({
+    'Phone Repair': false,
+    'Unlock devices': false,
+    'Sell & Buy': false,
+    'Review device': false,
+    'Phone compare': false,
+    'Other': false,
+  });
 
-  const formRef = useRef<HTMLFormElement>(null)
+  const [firstName,setFirstName] = useState('')
+  const [lastName,setLastName] = useState('')
+  const [email,setEmail] = useState('')
+  const [phoneNumber,setPhoneNumber] = useState('')
+  const [message,setMessage] = useState('')
+  const [error,setError] = useState('')
 
 
+console.log(firstName);
+
+  // after clicking send message
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault(); // Prevents the default form submission behavior
     alert('Message sent ✅');
-    if(formRef.current){
-      const inputs = formRef.current.querySelectorAll('input, textarea',);
-      inputs.forEach(input => {
-        (input as HTMLInputElement).value='';
-        
-        if (input instanceof HTMLInputElement && (input.type === 'checkbox' || input.type === 'radio')) {
-          input.checked = false; // Uncheck checkboxes and radio buttons
-        }
-      })
-    }
-
+    clearCheckboxes()
+    clearInputs()
   };
+
+  // Handle checkbox change
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, checked } = event.target;
+    setServices((prevServices) => ({
+      ...prevServices,
+      [name]: checked,
+    }));
+  };
+  // Clear all checkboxes
+  const clearCheckboxes = () => {
+    setServices({
+      'Phone Repair': false,
+      'Unlock devices': false,
+      'Sell & Buy': false,
+      'Review device': false,
+      'Phone compare': false,
+      'Other': false,
+    });
+  };
+
+  const clearInputs = ()=> {
+    setEmail('')
+    setFirstName('')
+    setLastName('')
+    setPhoneNumber('')
+    setMessage('')
+  }
 
   return (
     <section className=' bg-[#212121] p-8'>
+            <Element name='Contact'></Element>
         {/* Header + description */}
         <div className="text-center">
 
@@ -35,50 +74,60 @@ function Contact() {
     <div className='grid grid-cols-2 gap-4 justify-items-center m-auto text-white p-8 mt-8 max-w-[1000px]'>
 
     {/* form */}
-    <form onSubmit={handleSubmit} className='form' ref={formRef}>
+    <form onSubmit={handleSubmit} className='form'>
     {/* name */}
     <div className='grid grid-cols-2 gap-2 mb-[16px]'>
 
       <div className='flex flex-col '>
       <label htmlFor='firsName' className='mb-[10px] ml-[8px]'>First Name</label>
-      <input type="text" name='firsName' placeholder='First Name' required
-             className='input w-[80%]'/>
+      <input type="text" name='firsName' placeholder='First Name' value={firstName} required
+             className='input w-[80%]' onChange={(e)=>setFirstName(e.target.value.toUpperCase())}/>
       </div>
 
       <div className='flex flex-col'>
       <label htmlFor='lastName' className='mb-[10px] ml-[8px]'>Last Name</label>
-      <input type="text" placeholder='Last Name' required
-             className='input w-[80%]' />
+      <input type="text" placeholder='Last Name' value={lastName} required
+             className='input w-[80%]' onChange={(e)=>setLastName(e.target.value)} />
       </div>
     </div>
 
     <div className="flex flex-col mb-10">
     <label htmlFor='email' className='mb-[8px] ml-[8px]'>Email</label>
-    <input type="email" placeholder='you@company.com' required
-           className='w-[90%] input' />
+    <input type="email" placeholder='you@company.com' value={email} required
+           className='w-[90%] input' onChange={(e)=>setEmail(e.target.value)} />
     </div>
     <div className="flex flex-col mb-10">
     <label htmlFor='phoneNumber' className='mb-[10px] ml-[8px]'>Phone Number</label>
-    <input type="number" placeholder='+1(555) 000-0000'
+    <input type="number" placeholder='+1(555) 000-0000' value={phoneNumber}
            className='w-[90%] input [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none
-                      [&::-webkit-inner-spin-button]:appearance-none ' />
+                      [&::-webkit-inner-spin-button]:appearance-none ' onChange={(e)=>setPhoneNumber(e.target.value)}/>
     </div>
     
     <div className="flex flex-col mb-10">
     <label htmlFor='message' className='mb-[10px] ml-[8px]'>Message</label>
-    <textarea name="message" id="message" placeholder='Leave us a message' required
-              className='w-[90%] resize-none overflow-hidden h-[100px] input'></textarea>
+    <textarea name="message" id="message" placeholder='Leave us a message' value={message} required
+              className='w-[90%] resize-none overflow-hidden h-[100px] input'
+              onChange={(e)=>{
+                setMessage(e.target.value)
+                setError(e.target.value.includes('@')?'':'Invalid Email')
+              }
+              }></textarea>
     </div>
     <div className="lex flex-col mb-10">
     <label htmlFor='services' className='mb-[10px] ml-[8px] block'>Services</label>
     <div className='grid grid-cols-2 gap-2 w-[80%]'>
-      <div><input type="checkbox"/> Phone Repair </div>
-      <div><input type="checkbox"/> Unlock devices </div>
-      <div><input type="checkbox"/> Sell & Buy </div>
-      <div><input type="checkbox"/> Review device </div>
-      <div><input type="checkbox"/> Phone compare</div>
-      <div><input type="checkbox"/> Other </div>
-
+    {Object.entries(services).map(([service, isChecked]) => (
+          <div key={service}>
+            <input
+              type="checkbox"
+              name={service}
+              checked={isChecked}
+              onChange={handleCheckboxChange}
+              className='mr-2 cursor-pointer'
+            /> 
+            {service}
+          </div>
+        ))}
     </div>
 
     </div>
@@ -98,7 +147,7 @@ function Contact() {
         </div>
         <div className='flex gap-2 py-2 items-center'>
         <Mail size={'20px'} />
-        <a href="#" className='font-[600] border-b-1 text-[14px]'>Shoot us an Email </a>
+        <a href="mailto:samobile34@gmail.com" className='font-[600] border-b-1 text-[14px]'>Shoot us an Email </a>
         </div>
 
         <div className='flex gap-2 py-2 items-center'>
@@ -109,7 +158,7 @@ function Contact() {
 
       <div className='mb-4'>
       <h3 className='font-semibold text-[24px] mb-[10px]'>Call us</h3>
-      <p className='text-[14px] mb-[14px] text-gray-200'>Call our Team Sat-Thu from 9am to 9pm</p>
+      <p className='text-[14px] mb-[14px] text-gray-200'>Call our Team Sat-Thu from 9 am to 9 pm</p>
       <div className='flex gap-2 py-2 items-center'>
         <PhoneCall size={'20px'} />
         <a href="tel:+213541783626" className='font-[600] border-b-1 text-[14px]'>+213 (541)-78-3626</a>
@@ -131,8 +180,3 @@ function Contact() {
 }
 
 export default Contact
-
-// Next Step:
-// 01. continue designing the contact page
-// 02. start by the form flex ...
-// 03. delete number adder in the input.
