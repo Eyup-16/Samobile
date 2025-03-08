b24b7c9c6d3f6cd53cc2f74e3576a120691bdb6d
Eyup <171042578+Eyup-16@users.noreply.github.com>
2025-03-08 12:46:26 +0100

    Update Services.jsx
    under construction

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {ServiceCards} from './Cards'
 import { Wrench,KeyRound,DollarSign ,Cog   } from 'lucide-react';
-
+import { Element } from 'react-scroll';
 function Services() {
   const servicesObj = [
     {
@@ -24,14 +24,7 @@ function Services() {
       Header:'Buy, Sell & Trade-In',
       Description:'Sell your old phone or trade it in for a new one. Get instant cash or credit.',
       Button:'Learn More'
-    },
-    // {
-    //   id:2,
-    //   Icon:<Cog className='text-white h-8 w-8'/>,
-    //   Header:'Accessories & Customization',
-    //   Description:'Premium accessories & custom setups. Make your phone truly yours.',
-    //   Button:'Learn More'
-    // },
+    }
   ]
   const servicesCard =servicesObj.map((service)=> {
    return <ServiceCards key={service.id} icon={service.Icon} header={service.Header} desc={service.Description} btn={service.Button} />
@@ -39,8 +32,14 @@ function Services() {
   
   return (
     <section className='bg-gray-600 relative p-8 text-center'>
-    <h1 className='text-4xl font-semibold text-[#CACBCD] text-center mb-8 py-2 border-b-2 inline-block'>Services</h1>
-      <div className="grid md:grid-cols-3 p-4  items-center justify-items-center">
+      <Element name='Services'></Element>
+      <div className="text-center">
+
+<h1 className='text-4xl font-semibold text-[#CACBCD] text-center mb-8 py-2 border-b-2 inline-block'>Our Services</h1>
+<span className="text-[18px] font-semibold text-[#CACBCD]  cursor-pointer block">
+    Find the Service you looking for!</span>
+    </div>
+      <div className="grid md:grid-cols-3 p-4  items-center justify-items-center mt-4">
      {servicesCard}
     </div>
     </section>
