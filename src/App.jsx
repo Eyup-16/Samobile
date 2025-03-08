1afca56327260dc88f9b8904e0c2c236a4939d0c
Eyup <171042578+Eyup-16@users.noreply.github.com>
2025-03-08 12:47:56 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import NavBar from './Components/NavBar';
 import ShowCase from './Components/ShowCase';
-import Services from './Components/Services';
+import Services from './Components/services';
 import HelpCenter from './Components/HelpCenter';
 import Contact from './Components/Contact';
 import About from './Components/About';
+import Footer from './Components/Footer';
 function App() {
 
   return (
@@ -12,9 +13,10 @@ function App() {
      <NavBar/>
      <ShowCase/>
     <Services/>
-    <HelpCenter />
+    <HelpCenter/>
     <Contact/>
     <About/>
+    <Footer/>
     </main>
   )
 }
