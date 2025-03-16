3d60cb5004be2f8a83d7d630154cc38e35935c1e
Eyup <171042578+Eyup-16@users.noreply.github.com>
2025-03-16 01:57:32 +0100

    V1
    Official version v1.0

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,99 +1,59 @@
 import React from 'react'
-import {Facebook,Github, Instagram, Dribbble,Linkedin,Twitter} from 'lucide-react'
+import { serviceObj, socialMediaIcons } from '../data'
+import {ChevronDown} from 'lucide-react'
 function Footer() {
+    const [openAccordions, setOpenAccordions] = React.useState({});
 
-    const serviceObj =[
-        {
-          id:1,
-          title: 'Services',
-          links:['Overview', 'Repairs', 'Sell Your Phone', 'Warranty', 'Pricing', 'FAQs'],  
-        },
-        {
-          id:2,
-          title: 'Company',
-          links:['About Us', 'Careers', 'Press', 'Sustainability', 'Partners', 'Contact'],  
-        },
-        {
-          id:3,
-          title: 'Ressources',
-          links:['Repair Tips', 'Sell Guide', 'Troubleshooting', 'Blog', 'Videos', 'Support'],  
-        },
-        {
-          id:4,
-          title: 'Socials',
-          links:['Twitter', 'Linkedin', 'Facebook', 'Instagram', 'Github', 'Dribbble'],  
-        },
-        {
-          id:5,  
-          title: 'Legal',
-          links:['Privacy Policy', 'Terms of Service', 'Refund Policy', 'Warranty Terms', 'Accessibility', 'Sitemap'],  
-        },
-    ]
+    const toggleAccordion = (id:number) => {
+        setOpenAccordions((prev) => ({
+            ...prev,
+            [id]: !prev[id],
+        })); };  
 
-    const servicesTitles = serviceObj.map((service) =>{
+      const servicesTitles = serviceObj.map((service) =>{
         return(
-            <div key={service.id}> {/* items container */}
-        <h6 className='font-bold text-gray-400 mb-3 text-[14px]'>{service.title}</h6>
-        <ul>
-            {service.links.map((item)=>(<li key={item} className='mb-2'><a href="#" className=' hover:text-gray-300 transition-colors duration-200'>{item}</a></li>))}
+            <div key={service.id} className='w-full'> {/* items container */}
+
+           <input type="checkbox" className='invisible hidden peer' id={`accordion-${service.id}`} onChange={() => toggleAccordion(service.id)}/>
+            <label htmlFor={`accordion-${service.id}`} className="font-bold text-[#bbb] mb-4 text-[14px] max-md:text-[18px] max-md:items-center max-md:flex 
+            max-md:justify-between">
+            {service.title}
+            <ChevronDown size={20} strokeWidth={1.5} className={`md:hidden max-md:transition-transform max-md:duration-300 ${openAccordions[service.id]?' max-md:rotate-180':'max-md:rotate-0'}`} />
+                        </label>
+        <ul className='max-md:overflow-hidden max-md:h-0 max-md:transition-all peer-checked:max-md:duration-5000 peer-checked:max-md:h-auto '>
+            {service.links.map((item)=>(<li key={item} className='mb-2'><a href="#" className='transition-colors duration-200 hover:text-gray-300'>{item}</a></li>))}
         </ul>
     </div>
         )
     })
 
-
-    const socialMediaIcons =[
-        {
-            icon:<Twitter strokeWidth={1.75}/>,
-            URL:'https://X.com'
-        },
-        {
-            icon:<Linkedin strokeWidth={1.75}/>,
-            URL:'https://linkedin.com'
-        },
-        {
-            icon:<Facebook strokeWidth={1.75}/>,
-            URL:'https://facebook.com'
-        },
-        {
-            icon:<Instagram strokeWidth={1.75}/>,
-            URL:'https://instagram.com'
-        },
-        {
-            icon:<Github strokeWidth={1.75}/>,
-            URL:'https://github.com'
-        },
-        {
-            icon:<Dribbble strokeWidth={1.75}/>,
-            URL:'https://dribbble.com'
-        }
-    ]
     // SC is Social Media
     const SCMediaList = socialMediaIcons.map((SC)=>{
         return <li key={SC.URL} className='p-1 border-2 rounded-[6px] border-[#e5e5e5] '>
-            <a href={SC.URL}>{SC.icon}</a></li>
-
+            <a href={SC.URL} target='_blank'>{SC.icon}</a></li>
     })
 
   return (
 <footer className=' bg-[linear-gradient(to_bottom,_#222,_#333)] p-8 text-white'>
-   <div className='flex p-8 border-b-1'> {/* Container */}
+   <div className='flex md:p-8 border-b-1 max-md:flex-col'> {/* Container */}
 
-    <div className='flex flex-col w-[30%]'> {/* img & descrption */} 
-        <a href="#" className='cursor-auto'><img src="/Artboard 1.png" alt="logo" className='w-[120px] cursor-pointer' /></a>
-        <p className='max-w-[70%]'>Fixing phones, saving time, and putting smiles back in your hands.</p>
+    <div className='flex flex-col md:w-[30%] max-md:text-center max-md:items-center'> {/* img & descrption */} 
+        <a href="#" className='cursor-auto'><img src="/Artboard 1.png" alt="logo" className='w-[120px] cursor-pointer max-md:w-[150px]' /></a>
+        <p className='md:max-w-[70%] '>Fixing phones, saving time, and putting smiles back in your hands.</p>
     </div>
-    <div className="grid grid-cols-5 gap-5 w-[70%] mt-4">
+    {/* Loop over Services */}
+    <div className="grid md:grid-cols-5 md:gap-5 md:w-[70%] mt-4 max-md:mb-8 ">
         {servicesTitles}
     </div>
     </div> 
 
 {/* Real footer */}
-    <div className='p-8 flex justify-between items-center'>
-        <span className='font-semibold'>&copy; 2025 SAMobile LLC. All Rights Reserved.</span>
+    <div className='flex items-center justify-between md:p-8 max-md:py-8 max-md:flex-col'>
+        <span className='font-semibold max-md:mb-8 max-md:text-[15px]'>&copy; 2025 SAMobile LLC. All Rights Reserved.</span>
         <ul className='flex gap-4'>
             {SCMediaList}
         </ul>
+        <span className='p-2 text-center rounded-2xl max-md:mt-8 bg-gradient-to-r from-stone-500 to-white via-neutral-400'>Made with ❤️ by <a href="https://github.com/Eyup-16" target='_blank' className='font-bold text-[#444]'>Eyup</a></span>
     </div>
 </footer>
   )
