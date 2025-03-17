2aa05fce0d30990330dc63013e3be4f1fb118526
Eyup <171042578+Eyup-16@users.noreply.github.com>
2025-03-17 18:52:38 +0100

    Update and rename App.jsx to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
+import React from 'react'
 import NavBar from './Components/NavBar';
 import ShowCase from './Components/ShowCase';
-import Services from './Components/Services';
+import Services from './Components/Service';
 import HelpCenter from './Components/HelpCenter';
 import Contact from './Components/Contact';
 import About from './Components/About';
